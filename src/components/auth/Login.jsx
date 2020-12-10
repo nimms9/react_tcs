f19b3748e06e01d9528379479430bd7c94fc64fe
sudheer nimmagadda <snimmagadda2@student.gsu.edu>
2020-12-10 00:07:33 -0600

    updated now

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,36 @@
-import React, { Component } from "react";
+import axios from 'axios';
+import React, { useState } from 'react'
 
-export default class extends Component {
-  render() {
+
+export  const Login = () => {
+
+    
+    //const [data,setterfunction]=useState({initial data})
+    const [formData,setFormData]=useState({username:'',password:''});
+    const {username,password}= formData;
+    const handleChange = (event) => {
+       setFormData({...formData,[event.target.name]:event.target.value})
+      };
+    const  onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Final state :" + username);
+        console.log("password :"+password);
+        const loginUser = {
+          username: username,
+          password: password,
+        };
+        // we can pass 'formData' in axios.post
+        axios
+          .post("http://localhost:9050/api/auth/signin", loginUser)
+          .then((response) => {console.log(response.data)
+          localStorage.setItem('data',JSON.stringify(response.data))
+        })
+        .catch((err) => console.log(JSON.stringify(err)));
+    
+      };
+      
     return (
-      <div className="login">
+        <div className="login">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -11,13 +38,15 @@ export default class extends Component {
               <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
-              <form action="dashboard.html">
+              <form onSubmit={onSubmit}>
                 <div className="form-group">
                   <input
-                    type="email"
+                    type="text"
                     className="form-control form-control-lg"
-                    placeholder="Email Address"
-                    name="email"
+                    placeholder="Username"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
                   />
                 </div>
                 <div className="form-group">
@@ -26,14 +55,23 @@ export default class extends Component {
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
+                    value={password}
+                    onChange={handleChange}
                   />
                 </div>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-info btn-block mt-4"
+                />
               </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    )
 }
+
+export default Login;
+
+
